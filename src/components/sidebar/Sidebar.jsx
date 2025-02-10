85a67931d7eafe8a20f3587b560cdb1df8480044
JiJohnYuno <gng0202@naver.com>
2025-02-10 15:41:45 +0900

    Revert "commit"
    This reverts commit d4455f2e58c478f6ce6f535b8e82d3e0aaa52089.

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,44 +1,27 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Sidebar.css";
 import { AuthContext } from "../Context/AuthContext";
 
 function Sidebar() {
   const navi = useNavigate();
-  const { auth, logout } = useContext(AuthContext);
-  const [isAccordionOpen, setIsAccordionOpen] = useState(false);
 
   const goTo = (path) => {
     navi(path);
   };
 
-  const handleProfileClick = () => {
-    setIsAccordionOpen((prevState) => !prevState);
-    if (!auth.isAuthenticated) {
-      goTo("/login");
-    }
-  };
+  const { auth, logout } = useContext(AuthContext);
 
   return (
     <div className="sidebar">
       <h2 className="logoTitle">내가기린</h2>
       <h2>그린그림</h2>
 
-      <div className="profile-picture" onClick={handleProfileClick}>
-        <div className="start">
-          {auth.isAuthenticated ? "마이페이지" : "시작하기"}
-        </div>
-      </div>
+      {auth.isAuthenticated ? <></> : <></>}
 
-      {auth.isAuthenticated && (
-        <div className={`accordion-content ${isAccordionOpen ? "open" : ""}`}>
-          <ul>
-            <li onClick={logout}>로그아웃</li>
-            <li onClick={() => goTo("/mypage")}>프로필</li>
-            <li onClick={() => goTo("/settings")}>설정</li>
-          </ul>
-        </div>
-      )}
+      <div className="profile-picture" onClick={() => goTo("/login")}>
+        <div className="start">시작하기</div>
+      </div>
 
       <ul>
         <li onClick={() => goTo("/")}>홈버튼</li>
