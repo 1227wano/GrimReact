fb4cb8d02622ea22a6f60f9e1ffc28666c66dae8
JiJohnYuno <gng0202@naver.com>
2025-02-10 16:33:08 +0900

    sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,27 +1,47 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Sidebar.css";
 import { AuthContext } from "../Context/AuthContext";
 
 function Sidebar() {
   const navi = useNavigate();
+  const { auth, logout } = useContext(AuthContext);
+  const [isAccordionOpen, setIsAccordionOpen] = useState(false);
 
   const goTo = (path) => {
     navi(path);
   };
 
-  const { auth, logout } = useContext(AuthContext);
+  const handleProfileClick = () => {
+    if (auth.isAuthenticated) {
+      setIsAccordionOpen((prevState) => !prevState);
+    } else {
+      goTo("/login");
+    }
+  };
 
   return (
     <div className="sidebar">
       <h2 className="logoTitle">내가기린</h2>
       <h2>그린그림</h2>
 
-      {auth.isAuthenticated ? <></> : <></>}
-
-      <div className="profile-picture" onClick={() => goTo("/login")}>
-        <div className="start">시작하기</div>
+      <div className="profile-picture" onClick={handleProfileClick}>
+        <div className="start">
+          {auth.isAuthenticated ? "프로필사진들어갈곳" : "시작하기"}
+        </div>
       </div>
+
+      {auth.isAuthenticated && (
+        <div className={`accordion-content ${isAccordionOpen ? "open" : ""}`}>
+          <div className="accordion-inner">
+            <ul>
+              <li onClick={logout}>로그아웃</li>
+              <li onClick={() => goTo("/mypage")}>마이페이지</li>
+              <li onClick={() => goTo("/settings")}>설정</li>
+            </ul>
+          </div>
+        </div>
+      )}
 
       <ul>
         <li onClick={() => goTo("/")}>홈버튼</li>
