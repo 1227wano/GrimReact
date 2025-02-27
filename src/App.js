bc38edc09b9d7e0e1e2e5925c0e7a1bcc67ea518
JiJohnYuno <gng0202@naver.com>
2025-02-27 09:47:42 +0900

    commit
    commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import Sidebar from "./components/sidebar/Sidebar";
 import Footer from "./components/footer/Footer";
 import { Route, Routes } from "react-router-dom";
-
 import "./App.css";
 import Paint from "./components/painter/Paint";
 import { AuthProvider } from "./components/Context/AuthContext";
 import Login from "./components/Login/Login";
 import Signup from "./components/Signup/Signup";
 import MyPage from "./components/MyPage/MyPage";
-
 import Board from "./components/Board/Board";
-
 import MuseumForm from "./components/museum/MuseumForm";
 import MuseumMain from "./components/museum/MuseumMain";
 import MuseumDatail from "./components/museum/MuseumDetail";
 import MuseumUpdate from "./components/museum/MuseumUpdate";
+import Main from "./components/main/Main";
 
 function App() {
   return (
@@ -23,8 +21,9 @@ function App() {
         <Sidebar />
         <div className="content">
           <Routes>
+            <Route path="/" element={<Main />} />
             <Route path="/login" element={<Login />} />
-
+            <Route path="/main" element={<Main />} />
             <Route path="/paint" element={<Paint />} />
             <Route path="/members/signup" element={<Signup />} />
             <Route path="/mypage/*" element={<MyPage />} />
